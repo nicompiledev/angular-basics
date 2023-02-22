ffc8797d8507677ba929a27f7344d4c2b99b1ab1
nicompiledev <nsrivera80@soy.sena.edu.co>
2023-02-21 21:08:09 -0500

    practicing ngModel directive, pipe number, and @Input decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { BrowserModule } from '@angular/platform-browser'; // Angular platform m
 import { AppComponent } from './app.component'; // Import the app component
 import { CounterComponent } from './counter/counter.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './listado/listado.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({ // @NgModule decorator
   declarations: [ // The components that belong to this module
     AppComponent,
     CounterComponent,
     HeroeComponent,
-    ListadoComponent,
+
   ],
   imports: [ // The modules that belong to this module
-    BrowserModule
+    BrowserModule,
+    DbzModule
   ],
   providers: [], // The services that belong to this module
   bootstrap: [AppComponent] // The root component that Angular creates and inserts into the index.html host web page
